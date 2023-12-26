e898e66630830164f87fc90673878041ee08be12
badri vishal pandey <badri.p@coppermobile.com>
2023-12-26 15:44:08 +0530

    fjbdjbjd

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import firebase from './FirebaseConfig'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Firebase setup</h1></header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FirebaseConfig.js
@@ -0,0 +1,22 @@
+import { configure } from '@testing-library/react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+const config = {
+  apiKey: process.env.REACT_APP_API_Key , 
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN ,
+  projectId: process.env.REACT_APP_PROJECT_Id , 
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET , 
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID ,
+  appId: process.env.REACT_APP_APP_ID , 
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID ,
+}
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
+
+
+export default firebase;
